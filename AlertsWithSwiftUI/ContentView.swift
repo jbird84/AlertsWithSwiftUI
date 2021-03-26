09baef0a602bd39a74f2663955b22f1c11836c87
Josh Kinney <joshkinney@kinneykare.com>
2021-03-26 00:25:52 -0500

    Adding Alerts Project

--- AlertsWithSwiftUI/ContentView.swift
+++ AlertsWithSwiftUI/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isAlert = false
+    
+    var answers = ["No","Yes","Not Likely","I wouldn't bet on it","Absolutely","Certainly","100%", "Never", "Don't count on it"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            RadialGradient(gradient: Gradient(colors: [.pink, .black, .blue, .green, .gray, .orange, .purple, .pink, .black]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, startRadius: 10, endRadius: 450)
+                .ignoresSafeArea()
+            VStack {
+            Text("Ask Question!")
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                
+                Spacer()
+                
+                Button(action: {
+                    isAlert.toggle()
+                    print(isAlert)
+                }, label: {
+                    Text("Press For Answer")
+                })
+                .padding(.bottom, 180)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .alert(isPresented: $isAlert, content: {
+                    
+                    let alertButton = Alert.Button.default(Text("Ask Another Question"))
+                    
+                    return Alert(title: Text("\(answers.randomElement()!)"), message: Text("Press the Ask Another Question Button and ask another question."), dismissButton: alertButton)
+                })
+            }
+        }
     }
 }
 
