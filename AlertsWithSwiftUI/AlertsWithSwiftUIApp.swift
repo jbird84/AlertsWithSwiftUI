f5dc05ce94728d773fce1d32d87bda405c97e9de
Josh Kinney <joshkinney@kinneykare.com>
2021-03-25 22:31:22 -0500

    Initial Commit

---
+++ AlertsWithSwiftUI/AlertsWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsWithSwiftUIApp.swift
+//  AlertsWithSwiftUI
+//
+//  Created by Kinney Kare on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AlertsWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
